37f3c439d8ccc45eaf18695e54793965d0c5bf13
KrishPatel <krishvp10@gmail.com>
2026-06-19 12:40:09 +0530

    update: completed queries.py functions

--- db/queries.py
+++ db/queries.py
@@ -1,10 +1,8 @@
-import db.connection as db
-import yfinance as yf
+
+
 def insert_data(conn,symbol,df):
     cursor = conn.cursor()
-    Date =[]
-    for i in range(len(df["Date"])):
-        Date.append(df["Date"][i])
+    Date = list(df.index)
     Open = []
     for i in range(len(df["Open"])):
         Open.append(df["Open"][i])
@@ -21,8 +19,29 @@ def insert_data(conn,symbol,df):
     for i in range(len(df["Volume"])):
         Volume.append(df["Volume"][i])
     for i in range(len(Date)):
-        cursor.execute("INSERT INTO stock_data (symbol, date, open, high, low, close, volume) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+        cursor.execute("INSERT INTO stock_data (symbol, date, open, high, low, close, volume) VALUES (%s, %s, %s, %s, %s, %s, %s) ON CONFLICT (symbol, date) DO NOTHING",
                        (symbol, Date[i], Open[i], High[i], Low[i], Close[i], Volume[i]))
     conn.commit()
     cursor.close()
     
+
+def get_stock(conn,symbol):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM stock_data WHERE symbol = %s ORDER BY date",(symbol,))
+    data = cursor.fetchall()
+    cursor.close()
+    return data
+
+def latest_price(conn,symbol):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM stock_data WHERE symbol = %s ORDER BY date DESC LIMIT 1", (symbol,))
+    data = cursor.fetchone()
+    cursor.close()    
+    return data
+
+def symbol_exist(conn,symbol):
+    cursor = conn.cursor()
+    cursor.execute("SELECT COUNT(*) FROM stock_data WHERE symbol = %s",(symbol,))
+    count = cursor.fetchone()
+    cursor.close()
+    return count[0] > 0
