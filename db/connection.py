bb750641c031f2e363eca757cd430fc842b648cc
KrishPatel <krishvp10@gmail.com>
2026-06-23 13:09:05 +0530

    fix: connection.py with enviroment variable for deployment

--- db/connection.py
+++ db/connection.py
@@ -1,6 +1,13 @@
+import os
 import psycopg2 as pg
 
 def get_connection():
-    conn =  pg.connect("dbname=stock user=postgres password=krish@psql port=5432")
+    conn = pg.connect(
+        dbname=os.getenv("DB_NAME", "stock"),
+        user=os.getenv("DB_USER", "postgres"),
+        password=os.getenv("DB_PASSWORD", "krish@psql"),
+        host=os.getenv("DB_HOST", "localhost"),
+        port=os.getenv("DB_PORT", "5432")
+    )
     print("Connected to the database successfully")
     return conn
